cdfc65d2b093c9a6dff1d17b53788b2403a72cb7
Félix Bouazza <digitalmine.france@gmail.com>
2021-01-12 19:11:31 +0100

    upload create react application and install dep

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom"
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        
+      </div>
+
+    </Router>
+  );
+}
+
+export default App;
